47196a58b5aac33e5aac353e3c7e535ff76191a8
John MacLaughlin <johnmaclaughlin@gmail.com>
2020-12-12 15:16:58 -0800

    Add header

---
+++ src/home/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { AppHeader } from "./AppHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <AppHeader></AppHeader>
+      </header>
+    </div>
+  );
+}
+
+export default App;
